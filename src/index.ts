720a1f168ad2d5b7c3b35c3f64aa421b8258afe6
Taufik Oktama <oktamataufik@gmail.com>
2021-10-15 14:17:03 +0700

    [feat] Ignore lists

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
 export * from "./typetools";
-
-// load("font/PlusJakartaSans[wght].ttf").then((res) =>
-// 	console.log("ttf\t:", res)
-// );
-// load("font/PlusJakartaSans-Italic[wght].ttf").then((res) =>
-// 	console.log("ttf\t:", res)
-// );
