988e84f979e6d04464eec50237531a51c2d02142
Taufik Oktama <oktamataufik@gmail.com>
2021-10-15 14:01:46 +0700

    [fix] Comment initial fonts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { load } from "./typetools";
 export * from "./typetools";
 
-load("font/PlusJakartaSans[wght].ttf").then((res) =>
-	console.log("ttf\t:", res)
-);
-load("font/PlusJakartaSans-Italic[wght].ttf").then((res) =>
-	console.log("ttf\t:", res)
-);
+// load("font/PlusJakartaSans[wght].ttf").then((res) =>
+// 	console.log("ttf\t:", res)
+// );
+// load("font/PlusJakartaSans-Italic[wght].ttf").then((res) =>
+// 	console.log("ttf\t:", res)
+// );
